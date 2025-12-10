cfe0cc1ef264dfb1e133b16a0a7a037cdfd77241
NITIN RAJ <code.gecwc@gmail.com>
2025-12-10 19:36:10 +0530

    10-12-2025 (basic )

---
+++ own/hello.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class hello{
+    public static void main (String[] args)
+    {
+        int a=36;
+        if (a%2==0) {
+            System.out.println("even number ");
+        } else {
+            System.out.println("odd number ");
+        }
+    }
+}
